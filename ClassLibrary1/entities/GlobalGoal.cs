31a80cf3a5cad2e77eab1dfa48262243f33f9a83
aceymer <ljb@easv.dk>
2017-11-20 11:16:23 +0100

    added more

--- ClassLibrary1/entities/GlobalGoal.cs
+++ ClassLibrary1/entities/GlobalGoal.cs
@@ -7,7 +7,7 @@ namespace ClassLibrary1.entities
     public class GlobalGoal
     {
         public int Id { get; set; } 
-
+        
         public int TranslationId { get; set; }
         public Text Translation { get; set; }
         
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using ClassLibrary1;
+using ClassLibrary1.entities;
 using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 
@@ -17,6 +19,28 @@ namespace ConsoleApp1
                              .FirstOrDefault(l => l.LanguageISO == "US");
             Console.WriteLine("descr: " + ggText.Description);
             Console.WriteLine("title: " + ggText.Title);
+
+            var title = "Lækkert lækkert sted";
+            var description = "Der var sovs";
+            var ISO = "DK";
+
+            var contextSave = new ThaContext();
+            var lang = contextSave.Languages
+                            .FirstOrDefault(l => l.ISO == ISO);
+            var textLanguage = new TextLanguage() {
+                Language = lang,
+                Title = title,
+                Description = description
+            };
+            var dkGlobalGoal = new GlobalGoal() {
+                Translation = new Text() {
+                    Languages = new List<TextLanguage>() { textLanguage }
+                }
+            };
+
+            contextSave.GlobalGoals.Add(dkGlobalGoal);
+
+            contextSave.SaveChanges();
 
             Console.WriteLine("Waiting!!!!");
             Console.ReadLine();
